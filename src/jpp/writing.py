3ee25a69e5e506b1e1d18e77439986ee8cb3f74e
Peter Schmidbauer <peter.schmidb@gmail.com>
2019-11-17 20:55:51 +0100

    Add (de-)serialization of journals

--- src/jpp/writing.py
+++ src/jpp/writing.py
@@ -1,9 +1,10 @@
+import bisect
 import locale
 import re
 
 from datetime import datetime
 from pathlib import Path
-from typing import NamedTuple, Optional
+from typing import List, NamedTuple, Optional
 
 import dateparser
 
@@ -30,15 +31,56 @@ class Journal:
         journal_path = home / name
         return cls(journal_path)
 
+    def add(self, entry: Entry) -> None:
+        entries = self.read()
+        bisect.insort(entries, entry)  # sorted insert (sorts based on entry.date)
+
+        with self.path.open("a") as fp:
+            fp.write("\n\n")
+            fp.write(md_serialize(entry))
+
+    def read(self) -> List[Entry]:
+        with self.path.open("r") as fp:
+            journal_text = fp.read()
+
+        entry_texts = re.split(r"^## ", journal_text, flags=re.MULTILINE)
+        entry_texts = entry_texts[1:]  # skip first since it's an empty string
+        entries = [md_deserialize(text) for text in entry_texts]
+        return entries
+
 
 def md_serialize(entry: Entry) -> str:
     """Converts entry to markdown compatible string ready to write to file"""
     entry_date = entry.date.strftime(SERIALIZED_DATE_FORMAT)
-    entry_string = f"## {entry_date} {entry.title}"
+    entry_string = f"## {entry_date}: {entry.title}"
     if entry.body:
-        entry_string += "\n" + entry.body
+        # we use '## ' to denote a new entry, so we need to escape occurences of '#' in
+        # the body at the start of lines by adding two more '#'
+        body = re.sub(r"^#", "###", entry.body, flags=re.MULTILINE)
+        entry_string += "\n" + body
 
     return entry_string
+
+
+def md_deserialize(entry_text: str) -> Entry:
+    entry_text = entry_text.strip()
+    if not entry_text[0:3] == "## ":
+        raise ValueError(f"Cannot read entry, malformed:\n'{entry_text}'")
+
+    entry_text = entry_text[3:]
+    title_line, *body_lines = entry_text.split("\n")
+    date_str, title = title_line.split(": ")
+    date = datetime.strptime(date_str, SERIALIZED_DATE_FORMAT)
+    if not date:
+        raise ValueError(f"Cannot read entry, date missing:\n'{entry_text}'")
+
+    if not title:
+        raise ValueError(f"Cannot read entry, title missing:\n'{entry_text}'")
+
+    body = "\n".join(body_lines) if body_lines else ""
+    body = re.sub(r"^##(#*) ", r"\g<1> ", body, flags=re.MULTILINE)
+
+    return Entry(date, title, body)
 
 
 def parse_entry(text: str, date: Optional[datetime] = None) -> Entry:
@@ -62,12 +104,14 @@ def parse_entry(text: str, date: Optional[datetime] = None) -> Entry:
 
 
 def parse_date(date_string: str) -> datetime:
-    return dateparser.parse(date_string, locales=["en"])  # todo user preferences
+    user_locale = dateparse_time_locale() or "en"
+    return dateparser.parse(date_string, locales=[user_locale])
 
 
 def dateparse_time_locale() -> Optional[str]:
-    _ = locale.setlocale(locale.LC_ALL, "")
+    _ = locale.setlocale(locale.LC_ALL, "")  # needed to initialize locales
     lc_time = locale.getlocale(locale.LC_TIME)[0]
+    lc_time = lc_time.split(".")[0]  # remove potential encoding
 
     try:
         _ = dateparser.parse("01.01.2000", locales=[lc_time.replace("_", "-")])
--- tests/test_writing.py
+++ tests/test_writing.py
@@ -6,7 +6,7 @@ import hypothesis.strategies as st
 import pytest
 
 from hypothesis import assume, example, given
-from jpp.writing import parse_entry
+from jpp.writing import Entry, md_deserialize, md_serialize, parse_entry
 
 
 @pytest.fixture(autouse=True, scope="session")
@@ -19,14 +19,14 @@ def init_dateparser() -> None:
 
 @pytest.fixture(autouse=True)
 def lc_time(monkeypatch: Any) -> None:
-    monkeypatch.setenv("LC_TIME", "en_US")
+    monkeypatch.setenv("LC_TIME", "en_US.UTF-8")
 
 
 valid_dt_strings = st.sampled_from(
     ["today: ", "yesterday 5:30pm: ", "15th May 2019: ", "2099-09-09 9am: ", ""]
 )
 punctuation = st.text(alphabet=["!", ".", "?"])
-valid_title = st.text(
+valid_title_text = st.text(
     min_size=1,
     alphabet=st.characters(
         blacklist_categories=["Cs", "P"], blacklist_characters=["\n"]
@@ -35,30 +35,64 @@ valid_title = st.text(
 
 
 @st.composite
-def body(draw: Callable) -> None:
+def datetime_without_seconds(draw: Callable) -> datetime:
+    dt = draw(st.datetimes(min_value=datetime(1000, 1, 1)))
+    return dt.replace(second=0, microsecond=0)
+
+
+@st.composite
+def title(draw: Callable) -> str:
+    title_string = draw(valid_title_text).lstrip()
+    assume(title_string)
+    punct = draw(punctuation)
+    return (title_string + punct).rstrip()
+
+
+@st.composite
+def body(draw: Callable) -> str:
     """First char of body has to be non-whitespace and non-punctuation"""
     first_char = draw(st.characters(whitelist_categories=["L", "M", "S"]))
-    body = draw(st.text())
-    if body:
-        return first_char + body
+    body_string: str = draw(st.text())
+    if body_string:
+        return first_char + body_string.rstrip()
 
-    return body
+    return body_string
 
 
-@given(valid_dt_strings, valid_title, punctuation, body())
-@example(date="", title="not a date: bar", punct="", body="")
-def test_parse_valid_user_input(date: str, title: str, punct: str, body: str) -> None:
-    assume(not body or punct)
-    assume(title.strip())
+@given(valid_dt_strings, valid_title_text, punctuation, body())
+@example(date="", title_string="not a date: but title", punct="", body_string="")
+def test_parse_valid_user_input(
+    date: str, title_string: str, punct: str, body_string: str
+) -> None:
+    assume(not body_string or punct)
+    assume(title_string.strip())
 
-    user_input = f"{date}{title}{punct}{body}"
+    user_input = f"{date}{title_string}{punct}{body_string}"
     entry = parse_entry(user_input)
     dt = dateparser.parse(date[:-2], locales=["en"]) or datetime.now()
 
-    assert (
-        entry.date.year == dt.year
-        and entry.date.month == dt.month
-        and entry.date.day == dt.day
+    assert _dt_equals_in_minutes(entry.date, dt)
+    assert entry.title == (title_string + punct).strip()
+    assert entry.body == body_string.strip()
+
+
+@given(st.builds(Entry, datetime_without_seconds(), title(), body()))
+@example(
+    Entry(
+        datetime(2000, 1, 1), "foo", "## needs escaping\n#### this too\nbut not this ##"
     )
-    assert entry.title == (title + punct).strip()
-    assert entry.body == body.strip()
+)
+def test_md_serialization(entry: Entry) -> None:
+    serialized = md_serialize(entry)
+    deserialized = md_deserialize(serialized)
+
+    assert serialized.startswith("## ")
+    assert entry.title in serialized
+
+    assert _dt_equals_in_minutes(entry.date, deserialized.date)
+    assert entry.title == deserialized.title
+    assert entry.body == deserialized.body
+
+
+def _dt_equals_in_minutes(dt1: datetime, dt2: datetime) -> bool:
+    return dt1.replace(second=0, microsecond=0) == dt2.replace(second=0, microsecond=0)
